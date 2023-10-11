9591254000626fce1156cffe07d2cb3b926daed5
miguelvega <miguelvegasolde@gmail.com>
2023-10-10 21:43:53 -0500

    Pasando todas las pruebas

--- lib/activerecord_practica.rb
+++ lib/activerecord_practica.rb
@@ -21,7 +21,52 @@ class Customer < ActiveRecord::Base
     Customer.where(first: 'Candice')
   end
   def self.with_valid_email
-    # Tu codigo aqui para devolver solo clientes con direcciones de correo electronico validas (que contengan '@')
+    Customer.where("email LIKE '%@%'")
   end
-  # etc...
+
+  def self.with_dot_org_email
+    Customer.where("email LIKE '%.org'")
+  end
+
+  def self.with_invalid_email
+    Customer.where("email NOT LIKE '%@%' AND email IS NOT NULL AND email != ''")
+  end
+
+  def self.with_blank_email
+    Customer.where("email IS NULL OR email = ''")
+  end
+
+  def self.born_before_1980
+    Customer.where("birthdate < ?", Date.new(1980, 1, 1))
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    Customer.where("email LIKE '%@%' AND birthdate < ?", Date.new(1980, 1, 1))
+  end
+
+  def self.last_names_starting_with_b
+    Customer.where("last LIKE 'B%'").order(:birthdate)
+  end
+
+  def self.twenty_youngest
+    Customer.order(birthdate: :desc).limit(20)
+  end
+
+
+  def self.update_gussie_murray_birthdate
+    gussie = Customer.find_by(first: 'Gussie', last: 'Murray')
+    gussie.update(birthdate: Date.new(2004, 2, 8))
+  end
+
+  def self.change_all_invalid_emails_to_blank
+    Customer.where("email IS NOT NULL AND email NOT LIKE '%@%'").update_all(email: nil)
+  end
+
+  def self.delete_meggie_herman
+    Customer.find_by(first: 'Meggie', last: 'Herman')&.destroy
+  end
+
+  def self.delete_everyone_born_before_1978
+    Customer.where("birthdate < ?", Date.new(1978, 1, 1)).destroy_all
+  end
 end
--- spec/activerecord_practica_spec.rb
+++ spec/activerecord_practica_spec.rb
@@ -39,30 +39,30 @@ describe 'Practica ActiveRecord' do
       specify 'cualquier persona con el nombre Candice' do
         check Customer.any_candice, [24]
       end
-      xspecify 'con un correo electrónico válido (la dirección de correo electrónico contiene "@") (busca el operador SQL LIKE)' do
+      specify 'con un correo electrónico válido (la dirección de correo electrónico contiene "@") (busca el operador SQL LIKE)' do
         check Customer.with_valid_email, [1,2,4,5,7,8,10,11,12,13,14,15,17,18,19,20,23,26,29,30]
       end
-      xspecify 'con emails .org' do
+      specify 'con emails .org' do
         check Customer.with_dot_org_email, [5,7,8,12,23,26,29]
       end
-      xspecify 'con correo electronico no valido pero que no este en blanco (no contiene "@")' do
+      specify 'con correo electronico no valido pero que no este en blanco (no contiene "@")' do
         check Customer.with_invalid_email, [3,6,9,16,22,25,27,28]
       end
-      xspecify 'con correo electronico en blanco' do
+      specify 'con correo electronico en blanco' do
         check  Customer.with_blank_email, [21,24]
       end
-      xspecify 'nacido antes del 1 Enero 1980' do
+      specify 'nacido antes del 1 Enero 1980' do
         check Customer.born_before_1980, [3,8,9,11,15,16,17,19,20,24,25,27]
       end
-      xspecify 'con correo electronico valido Y nacidos antes del 1/1/1980' do
+      specify 'con correo electronico valido Y nacidos antes del 1/1/1980' do
         check Customer.with_valid_email_and_born_before_1980, [8,11,15,17,19,20]
       end
-      xspecify 'con apellidos que comienzan con "B", ordenados por fecha de nacimiento' do
+      specify 'con apellidos que comienzan con "B", ordenados por fecha de nacimiento' do
         expect(Customer.last_names_starting_with_b.map(&:id)).to eq( [25,23,4,28,18,21,29,1] )
       end
     end
     describe 'sin necesidad de .where' do
-      xspecify '20 clientes mas jovenes, en cualquier orden (busca ActiveRecord "order" y "limit")' do
+      specify '20 clientes mas jovenes, en cualquier orden (busca ActiveRecord "order" y "limit")' do
         check Customer.twenty_youngest, [7,5,6,30,1,10,29,21,18,13,14,28,26,4,2,22,23,12,11,9]
       end
     end
@@ -71,19 +71,19 @@ describe 'Practica ActiveRecord' do
     before(:each) do
       expect(Customer).not_to receive(:find)
     end
-    xspecify 'la fecha de nacimiento de Gussie Murray hasta el 8 de febrero de 2004 (busca `Time.parse`)'do
+    specify 'la fecha de nacimiento de Gussie Murray hasta el 8 de febrero de 2004 (busca `Time.parse`)'do
       Customer.update_gussie_murray_birthdate
       expect(Customer.find_by(:first => 'Gussie').birthdate.to_date).to eq(Date.new 2004,2,8)
     end
-    xspecify 'Todos los correos electronicos no validos deben estar en blanco.' do
+    specify 'Todos los correos electronicos no validos deben estar en blanco.' do
       Customer.change_all_invalid_emails_to_blank
       expect(Customer.where("email != '' AND email IS NOT NULL and email NOT LIKE '%@%'").count).to be_zero
     end
-    xspecify 'base de datos eliminando al cliente Meggie Herman' do
+    specify 'base de datos eliminando al cliente Meggie Herman' do
       Customer.delete_meggie_herman
       expect(Customer.find_by(:first => 'Meggie', :last => 'Herman')).to be_nil
     end
-    xspecify 'base de datos eliminando todos los clientes nacidos el 31 de diciembre de 1977' do
+    specify 'base de datos eliminando todos los clientes nacidos el 31 de diciembre de 1977' do
       Customer.delete_everyone_born_before_1978
       expect(Customer.where('birthdate < ?', Time.parse("1 January 1978"))).to be_empty
     end
